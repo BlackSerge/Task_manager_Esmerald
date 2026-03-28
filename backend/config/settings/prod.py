b1304cbabb403e68bb5952db96e67acf68e3451b
BlackSerge <sergiosaborit99@gmail.com>
2026-03-28 15:49:04 -0400

    fix: separate cache logic for local and prod with upstash ssl fix

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,14 +1,33 @@
 from .base import *
 
+
 DEBUG = False
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": env("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {
+                "ssl_cert_reqs": None, 
+                "retry_on_timeout": True,
+            },
+            "SOCKET_CONNECT_TIMEOUT": 10, 
+            "SOCKET_TIMEOUT": 10,
+        }
+    }
+}
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DATABASES["default"]["OPTIONS"] = {
     "sslmode": "require",
 }
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=True)
 SESSION_COOKIE_SECURE = True
@@ -18,7 +37,6 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-# Extra Security Headers
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
