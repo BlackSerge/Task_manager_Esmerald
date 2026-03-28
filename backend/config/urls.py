a864b9eeae9db5ac5b1ad790ea7245abde065cbf
BlackSerge <sergiosaborit99@gmail.com>
2026-03-28 15:35:44 -0400

    fix: docker pathing and add api health check root

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse 
+
+def api_health_check(request):
+    return JsonResponse({"status": "online", "message": "Collaborative Board API Working"})
 
 urlpatterns = [
+    path('', api_health_check),
     path('admin/', admin.site.urls),
     path('api/auth/', include('core.urls.urls_auth')),
     path('api/users/', include('core.urls.urls_users')),
